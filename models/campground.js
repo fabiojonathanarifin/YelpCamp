c60491220d3de1e84f79594388834af1c00e1e2a
Fabio Jonathan Arifin <42570291+fabiojonathanarifin@users.noreply.github.com>
2022-05-05 21:15:09 +0700

    Documentation + images added to show page

--- models/campground.js
+++ models/campground.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+//schema is the model object data for the server(basically like blueprint/template)
 const CampgroundSchema = new Schema({
     title: String,
-    price: String,
+    image: String,
+    price: Number,
     description: String,
     location: String
 });
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
+//this model is used in seeds/index.js to create seed data
+//this model is also used in app.js to access/recreate/manipulate previous data from the client-side (using CRUD)
--- seeds/index.js
+++ seeds/index.js
@@ -1,8 +1,11 @@
+//this file randomize and create/refresh data in the server(mongodb - db.campgrounds.find()) everytime it's launched
+
 const mongoose = require('mongoose');
 const cities = require('./cities');
 const {places, descriptors} = require('./seedHelpers');
 const Campground = require('../models/campground')
 
+//first time usage is to create yelp-camp app data in the server, the next usage is to connect to the yelp-camp database
 mongoose.connect('mongodb://localhost:27017/yelp-camp').
     catch(error => handleError(error));
 
@@ -12,19 +15,26 @@ db.once('open', () => {
     console.log('Database connected');
 });
 
+//randomizer function for title mix&match
 const sample = array => array[Math.floor(Math.random() * array.length)];	
+//create & randomize 50 data in mongodb server
 const seedDB = async () => {	
     await Campground.deleteMany({});	
     for (let i = 0; i < 50; i++) {	
         const random1000 = Math.floor(Math.random() * 1000);	
+        const price = Math.floor(Math.random() * 30) + 10;
         const camp = new Campground({	
             location: `${cities[random1000].city}, ${cities[random1000].state}`,	
-            title: `${sample(descriptors)} ${sample(places)}`	
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae aperiam nihil illum odio qui perspiciatis provident, consequatur nemo! Quidem fugit quibusdam minima amet quo quaerat mollitia numquam velit. Id, quasi!Lorem',
+            price
         })	
         await camp.save();	
     }	
 }
 
+//close the app right away
 seedDB().then(() => {
     mongoose.connection.close()
 });
