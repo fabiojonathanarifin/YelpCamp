57e1b5a697b90ad7cb1853d12b7a3a165c10be19
Fabio Jonathan Arifin <42570291+fabiojonathanarifin@users.noreply.github.com>
2022-05-16 21:08:21 +0700

    mongoose middleware setup to remove review when campground is removed

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Review = require('./review');
 const Schema = mongoose.Schema;
 
 //schema is the model object data for the server(basically like blueprint/template)
@@ -15,6 +16,21 @@ const CampgroundSchema = new Schema({
         }
     ]
 });
+
+// mongoose middleware, it will be engaged when a campground is deleted
+//the .post is not CRUD POST, it's pre vs post... means that the findOneAndDelete middleware will run after/post the campground deletion
+//the findOneAndDelete is the action that would trigger the async function
+// ---------- if something is deleted(findOneAndDelete is triggered by findByIdAndDelete), the async function will run
+CampgroundSchema.post('findOneAndDelete', async function(doc) {
+    if(doc){
+        //this means that we will delete all reviews their id field is in the doc and its reviews array
+        await Review.remove({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
 //this model is used in seeds/index.js to create seed data
