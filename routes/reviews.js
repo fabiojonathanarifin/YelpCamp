3269a32069654e9ff0bc3935bc1bc34bc1c78f91
fabiojonathanarifin <42570291+fabiojonathanarifin@users.noreply.github.com>
2022-06-10 23:37:26 +0700

    review.author on revies.js route.post created

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { validateReview } = require('../middleware')
+const { validateReview, isLoggedIn } = require('../middleware')
 //router instead of app
 const router = express.Router({ mergeParams: true });
 const Campground = require('../models/campground');
@@ -7,10 +7,11 @@ const Review = require('../models/review')
 const catchAsync = require('../utils/catchAsync')
 
 
-
-router.post('/', validateReview, catchAsync(async (req, res) => {
+router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     const review = new Review(req.body.review)
+    //embed user id in the review
+    review.author = req.user._id
     campground.reviews.push(review);
     await review.save();
     await campground.save();
