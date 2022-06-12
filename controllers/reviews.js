22bf95ff2028b02b4c751a1c026d7255cbf9409e
fabiojonathanarifin <42570291+fabiojonathanarifin@users.noreply.github.com>
2022-06-12 17:16:35 +0700

    reviews routes to reviews controllers

---
+++ controllers/reviews.js
@@ -0,0 +1,23 @@
+const Campground = require('../models/campground');
+const Review = require('../models/review')
+
+module.exports.createReview = async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    const review = new Review(req.body.review)
+    //embed user id in the review
+    review.author = req.user._id
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    req.flash('success', 'Created new review!')
+    res.redirect(`/campgrounds/${campground._id}`)
+}
+
+module.exports.deleteReview = async (req, res) => {
+    const { id, reviewId } = req.params;
+    //$pull is used to delete the review without deleting the entire campground. it will delete the one specifically with the given id
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } })
+    await Review.findByIdAndDelete(reviewId)
+    req.flash('success', 'Successfully deleted a review')
+    res.redirect(`/campgrounds/${id}`)
+}
