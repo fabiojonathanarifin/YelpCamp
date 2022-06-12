28fc5a5b45764a090c75100c349eb42de75090c1
fabiojonathanarifin <42570291+fabiojonathanarifin@users.noreply.github.com>
2022-06-12 17:59:07 +0700

    refactoring implement router.route

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,22 +4,22 @@ const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utils/catchAsync');
 
-//campgrounds.index, located in controllers campgrounds, module.exports.index
-router.get('/', catchAsync(campgrounds.index));
+router.route('/')
+    //campgrounds.index, located in controllers campgrounds, module.exports.index
+    .get(catchAsync(campgrounds.index))
+    //adding new campground to the server and displaying it on campground
+    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
 
 // page for inputing new data into the server
 router.get('/new', isLoggedIn, campgrounds.renderNewForm)
 
-//adding new campground to the server and displaying it on campground
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
 
-//showing specific place in the list
-router.get('/:id', catchAsync(campgrounds.showCampground))
+router.route('/:id')
+    //showing specific place in the list
+    .get(catchAsync(campgrounds.showCampground))
+    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground))
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm))
-
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
-
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground))
 
 module.exports = router;
