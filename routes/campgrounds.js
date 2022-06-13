736382ca27a7200ffd04b46da553927b619d3f36
fabiojonathanarifin <42570291+fabiojonathanarifin@users.noreply.github.com>
2022-06-13 20:54:48 +0700

    install and using multer - a middleware to parse image

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,12 +3,18 @@ const router = express.Router();
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utils/catchAsync');
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 router.route('/')
     //campgrounds.index, located in controllers campgrounds, module.exports.index
     .get(catchAsync(campgrounds.index))
     //adding new campground to the server and displaying it on campground
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+    // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+    // upload.single and req.file is from multer
+    .post(upload.single('image'), (req, res) => {
+        res.send(req.body, req.file)
+    })
 
 // page for inputing new data into the server
 router.get('/new', isLoggedIn, campgrounds.renderNewForm)
