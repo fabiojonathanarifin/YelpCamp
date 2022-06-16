b40df4d8566e610b83060114f39a4583df2ed3f8
fabiojonathanarifin <42570291+fabiojonathanarifin@users.noreply.github.com>
2022-06-16 13:42:39 +0700

    formatting

--- middleware.js
+++ middleware.js
@@ -1,63 +1,64 @@
-const { campgroundSchema, reviewSchema } = require('./schemas.js')
-const ExpressError = require('./utils/ExpressError')
-const Campground = require('./models/campground');
-const Review = require('./models/review.js');
+const { campgroundSchema, reviewSchema } = require("./schemas.js");
+const ExpressError = require("./utils/ExpressError");
+const Campground = require("./models/campground");
+const Review = require("./models/review.js");
 
 //server side validation using Joi for review
 module.exports.validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next()
-    }
-}
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
 
 //req,res,next is necessary because this is a middleware
 module.exports.isLoggedIn = (req, res, next) => {
-    //isAuthenticated is helper method from passport
-    //it check whethe client has logged in or not
-    if (!req.isAuthenticated()) {
-        const { id } = req.params
-        //returning to the client's destination after logging in
-        req.session.returnTo = (req.query._method === 'DELETE' ? `/campgrounds/${id}` : req.originalUrl);
-        req.flash('error', 'you must be signed in first');
-        return res.redirect('/login');
-    }
-    next();
-}
+  //isAuthenticated is helper method from passport
+  //it check whethe client has logged in or not
+  if (!req.isAuthenticated()) {
+    const { id } = req.params;
+    //returning to the client's destination after logging in
+    req.session.returnTo =
+      req.query._method === "DELETE" ? `/campgrounds/${id}` : req.originalUrl;
+    req.flash("error", "you must be signed in first");
+    return res.redirect("/login");
+  }
+  next();
+};
 
 //server side validation using Joi for error handling purposes
 module.exports.validateCampground = (req, res, next) => {
-    const { error } = campgroundSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next()
-    }
-}
+  const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
 
 //validation check for the author to delete and edit camprgound
 module.exports.isAuthor = async (req, res, next) => {
-    const { id } = req.params;
-    const campground = await Campground.findById(id);
-    if (!campground.author.equals(req.user._id)) {
-        req.flash('error', 'You do not have permission to do that!');
-        return res.redirect(`/campgrounds/${id}`)
-    }
-    next();
-}
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  if (!campground.author.equals(req.user._id)) {
+    req.flash("error", "You do not have permission to do that!");
+    return res.redirect(`/campgrounds/${id}`);
+  }
+  next();
+};
 
 module.exports.isReviewAuthor = async (req, res, next) => {
-    //we use /:reviewId param on the reviews route
-    //the id is for the final route (res.redirect)
-    const { id, reviewId } = req.params;
-    const review = await Review.findById(reviewId);
-    if (!review.author.equals(req.user._id)) {
-        req.flash('error', 'You do not have permission to do that!');
-        return res.redirect(`/campgrounds/${id}`)
-    }
-    next();
-}
+  //we use /:reviewId param on the reviews route
+  //the id is for the final route (res.redirect)
+  const { id, reviewId } = req.params;
+  const review = await Review.findById(reviewId);
+  if (!review.author.equals(req.user._id)) {
+    req.flash("error", "You do not have permission to do that!");
+    return res.redirect(`/campgrounds/${id}`);
+  }
+  next();
+};
--- models/campground.js
+++ models/campground.js
@@ -1,47 +1,47 @@
-const mongoose = require('mongoose');
-const Review = require('./review');
+const mongoose = require("mongoose");
+const Review = require("./review");
 const Schema = mongoose.Schema;
 
 //schema is the model object data for the server(basically like blueprint/template)
 const CampgroundSchema = new Schema({
-    title: String,
-    //multiple images
-    images: [
-        {
-            url: String,
-            filename: String
-        }
-    ],
-    price: Number,
-    description: String,
-    location: String,
-    author: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
+  title: String,
+  //multiple images
+  images: [
+    {
+      url: String,
+      filename: String,
     },
-    reviews: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Review'
-        }
-    ]
+  ],
+  price: Number,
+  description: String,
+  location: String,
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
 });
 
 // mongoose middleware, it will be engaged when a campground is deleted
 //the .post is not CRUD POST, it's pre vs post... means that the findOneAndDelete middleware will run after/post the campground deletion
 //the findOneAndDelete is the action that would trigger the async function
 // ---------- if something is deleted(findOneAndDelete is triggered by findByIdAndDelete), the async function will run
-CampgroundSchema.post('findOneAndDelete', async function (doc) {
-    if (doc) {
-        //this means that we will delete all reviews their id field is in the doc and its reviews array
-        await Review.deleteMany({
-            _id: {
-                $in: doc.reviews
-            }
-        })
-    }
-})
+CampgroundSchema.post("findOneAndDelete", async function (doc) {
+  if (doc) {
+    //this means that we will delete all reviews their id field is in the doc and its reviews array
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
+});
 
-module.exports = mongoose.model('Campground', CampgroundSchema);
+module.exports = mongoose.model("Campground", CampgroundSchema);
 //this model is used in seeds/index.js to create seed data
 //this model is also used in app.js to access/recreate/manipulate previous data from the client-side (using CRUD)
