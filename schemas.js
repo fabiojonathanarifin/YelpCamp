f7b2a1f5d9cd3111ba787d93cf887849191897e8
fabiojonathanarifin <42570291+fabiojonathanarifin@users.noreply.github.com>
2022-06-20 16:21:17 +0700

    delete images fe form

--- schemas.js
+++ schemas.js
@@ -1,19 +1,19 @@
-const Joi = require('Joi');
-
+const Joi = require("Joi");
 
 module.exports.campgroundSchema = Joi.object({
-    campground: Joi.object({
-        title: Joi.string().required(),
-        price: Joi.number().required().min(0),
-        // image: Joi.string().uri().required(),
-        location: Joi.string().required(),
-        description: Joi.string().required()
-    }).required()
-})
+  campground: Joi.object({
+    title: Joi.string().required(),
+    price: Joi.number().required().min(0),
+    // image: Joi.string().uri().required(),
+    location: Joi.string().required(),
+    description: Joi.string().required(),
+  }).required(),
+  deleteImages: Joi.array(),
+});
 
 module.exports.reviewSchema = Joi.object({
-    review: Joi.object({
-        rating: Joi.number().required().min(1).max(5),
-        body: Joi.string().required()
-    }).required()
-})
+  review: Joi.object({
+    rating: Joi.number().required().min(1).max(5),
+    body: Joi.string().required(),
+  }).required(),
+});
