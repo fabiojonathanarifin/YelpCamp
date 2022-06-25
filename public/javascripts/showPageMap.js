c3ac733b34bb679df77ea35a14d37d6236c25b81
fabiojonathanarifin <42570291+fabiojonathanarifin@users.noreply.github.com>
2022-06-25 22:09:44 +0700

    finish geolocation

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,10 +1,7 @@
-const { shallowCopyFromList } = require("ejs/lib/utils");
-
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: "map", // container ID
   style: "mapbox://styles/mapbox/streets-v11", // style URL
-  //campground.geometry.coordinates data comes from show.ejs campground script
   center: campground.geometry.coordinates, // starting position [lng, lat]
   zoom: 9, // starting zoom
 });
