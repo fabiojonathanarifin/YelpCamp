fa3d5b8ebfc68e8057bf9d3f4cf52c6698caf4a1
fabiojonathanarifin <42570291+fabiojonathanarifin@users.noreply.github.com>
2022-06-26 16:57:25 +0700

    fix geometry bug

--- seeds/index.js
+++ seeds/index.js
@@ -25,6 +25,7 @@ const seedDB = async () => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 30) + 10;
     const camp = new Campground({
+      //YOUR USER ID
       author: "629cb59184d5f4f988134435",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
