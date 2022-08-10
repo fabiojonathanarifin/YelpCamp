13ec3c9578f4012ad2e14847d007519131b5e51e
fabiojonathanarifin <42570291+fabiojonathanarifin@users.noreply.github.com>
2022-08-10 18:45:08 +0700

    post bug fixed

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,13 +1,18 @@
-const express = require('express');
-const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware')
+const express = require("express");
+const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware");
 //router instead of app
 const router = express.Router({ mergeParams: true });
-const reviews = require('../controllers/reviews')
-const catchAsync = require('../utils/catchAsync')
+const reviews = require("../controllers/reviews");
+const catchAsync = require("../utils/catchAsync");
 
-router.post('/', isLoggedIn, validateReview, catchAsync(reviews.createReview))
+router.post("/", isLoggedIn, validateReview, catchAsync(reviews.createReview));
 
-//the path /campgrounds/:id/rev, etc. is the url, it can be modivied; it doesn't refer to any directory or index pages
-router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview))
+//the path /campgrounds/:id/rev, etc. is the url, it can be modified; it doesn't refer to any directory or index pages
+router.delete(
+  "/:reviewId",
+  isLoggedIn,
+  isReviewAuthor,
+  catchAsync(reviews.deleteReview)
+);
 
 module.exports = router;
